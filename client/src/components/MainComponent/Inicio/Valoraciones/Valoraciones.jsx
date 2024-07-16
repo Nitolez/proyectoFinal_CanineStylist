cc1a8dae525ebb7a2723fc61dd865f619adfc06d
Nitolez <antonioglezt98@gmail.com>
2024-07-16 22:44:17 +0200

    Reseñas de google preview

--- client/src/components/MainComponent/Inicio/Valoraciones/Valoraciones.jsx
+++ client/src/components/MainComponent/Inicio/Valoraciones/Valoraciones.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 
 const ListaValoraciones = () => {
 
-  return (
-  <h2>Valoraciones</h2>
-)};
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://widgets.sociablekit.com/google-reviews/widget.js';
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
 
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className='sk-ww-google-reviews' data-embed-id='25438135'></div>
+  );
+};
 export default ListaValoraciones;
