5be237e16072b411d7b356dd9f9e0e3fc90796c4
Nitolez <antonioglezt98@gmail.com>
2024-07-20 13:46:52 +0200

    Ajustes para despliegue

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express(); // Inicializar servidor
 const port = 3000;
-const path = require('path');
 const cors =  require('cors');
 
 app.use(cors());
 
 //Importar middlewares
 const error404 = require("./middlewares/error404");
-app.use(express.static(path.join(__dirname, 'client/dist')));
-
 
 // Importar Rutas API
 const rutasRazas = require('./routes/razas.routes');
@@ -28,9 +25,6 @@ app.use('/api/usuarios', rutasUsuarios);
 //Invocar middleware
 app.use(error404); //Middleware para manejo de 404
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
-  });
 
 const server = app.listen(port, () => { // Servidor está escuchando en este puerto variable port
     console.log(`Example app listening on http://localhost:${port}`);
